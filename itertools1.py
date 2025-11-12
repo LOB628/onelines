b19517670db3efd282b21ab806658b4f0c6f3e0a
Liam Baird <liambaird628@gmail.com>
2025-11-11 23:17:55 -0500

    fixes for batched, added chain,compress,dropwhile,filterfalse,takewhile

--- itertools1.py
+++ itertools1.py
@@ -14,7 +14,7 @@ def count2(start, step=1):
 
 
 def cycle(p):
-    # note False is used as a sentinel since it is never yielded by the inner generator
+    # note False is used as a sentinel since it is never yielded by the inner generator to avoid unnecessary use of __SENTINEL = object()
     return (i for i in p for _ in iter(lambda: True, False))
 
 
@@ -22,8 +22,8 @@ def cycle2(p):
     (yield from p), (yield from cycle2(p))
 
 
-def repeat(elem, n=None):
-    return (elem for _ in range(n)) if n is not None else iter(lambda: elem, None)
+def repeat(elem, n=None, __SENTINEL=object()):
+    return (elem for _ in range(n)) if n is not None else iter(lambda: elem, __SENTINEL)
 
 
 def accumulate(
@@ -39,9 +39,49 @@ def accumulate(
     )
 
 
-def batched(p, n, __SENTINEL=object()):
+def batched(p, n):
     (
         (iterator := iter(p)),
-        (yield tuple(iter((next(iterator, __SENTINEL) for _ in range(n)), __SENTINEL))),
-        (yield from batched(iterator, n)),
+        (ret := tuple((elem for _, elem in zip(range(n), iterator)))),
+        ((yield ret), (yield from batched(iterator, n))) if ret else None,
+    )
+
+
+def chain(*iterables):
+    return (element for p in iterables for element in p)
+
+
+def chain_from_iterable(iterable):
+    return (element for p in iterable for element in p)
+
+
+def compress(data, selectors):
+    return (d for d, s in zip(data, selectors) if s)
+
+
+def dropwhile(predicate, iterable):
+    (
+        (iterator := iter(iterable)),
+        (yield next(element for element in iterator if not predicate(element))),
+        (yield from iterator),
+    )
+
+
+def filterfalse(predicate, iterable):
+    return (
+        (element for element in iterable if not predicate(element))
+        if predicate is not None
+        else filterfalse(bool, iterable)
+    )
+
+
+def takewhile(predicate, iterable, __SENTINEL=object()):
+    return iter(
+        (
+            lambda iterator=(
+                elem if pred else __SENTINEL
+                for elem, pred in zip(iterable, map(predicate, iterable))
+            ): next(iterator, __SENTINEL)
+        ),
+        __SENTINEL,
     )
